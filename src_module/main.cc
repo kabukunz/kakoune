759ab3863088f9fc205a68fa5b5c292780ea00f3
Tony Narlock <tony@git-pull.com>
2016-06-02 14:15:36 -0500

    milk a variable out of kakoune

--- src_module/main.cc
+++ src_module/main.cc
@@ -1,6 +1,8 @@
 #include <pybind11/pybind11.h>
 #include "ncurses_ui.hh"
 #include "ncurses.h"
+#include "file.hh"
+#include "string.hh"
 
 int add(int i, int j) {
     return i + j;
@@ -9,9 +11,14 @@ int add(int i, int j) {
 namespace py = pybind11;
 
 PYBIND11_PLUGIN(kakoune) {
+
     py::module m("kakoune", "pybind11 example plugin");
 
     m.def("add", &add, "A function which adds two numbers");
+    m.def("get_kak_binary_path", []() {
+            std::string thing = std::string(reinterpret_cast<const char*>(Kakoune::get_kak_binary_path().data()));
+            return thing;
+    }, "A function which adds two numbers");
 
     return m.ptr();
 }
