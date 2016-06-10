49d6d7f379d9ddb9ef6f2bc88dc271d8412b3ff9
Tony Narlock <tony@git-pull.com>
2016-06-10 16:35:53 -0500

    Add main.cc with sample PyObject

--- src/main.cc
+++ src/main.cc
@@ -15,6 +15,7 @@
 #include "register_manager.hh"
 #include "unit_tests.hh"
 #include "window.hh"
+#include "python.h"
 
 #include <fcntl.h>
 #include <unistd.h>
@@ -660,7 +661,37 @@ int cli(int argc, char* argv[])
     return 0;
 }
 
-int main(int argc, char* argv[])
-{
-    return cli(argc, argv);
+static PyObject* spam_system(PyObject* self, PyObject* args) {
+  const char* command;
+  int sts;
+
+  if (!PyArg_ParseTuple(args, "s", &command))
+    return NULL;
+  sts = system(command);
+  return PyLong_FromLong(sts);
+}
+
+static PyMethodDef SpamMethods[] = {
+    {"system", spam_system, METH_VARARGS, "Execute a shell command."},
+    {NULL, NULL, 0, NULL} /* Sentinel */
+};
+
+static struct PyModuleDef spammodule = {
+    PyModuleDef_HEAD_INIT, "spam", /* name of module */
+    nullptr,                       /* module documentation, may be NULL */
+    -1, /* size of per-interpreter state of the module,
+           or -1 if the module keeps state in global variables. */
+    SpamMethods};
+
+PyMODINIT_FUNC PyInit_spam(void) {
+  return PyModule_Create(&spammodule);
+}
+
+int main(int argc, char* argv[]) {
+  PyImport_AppendInittab("spam", PyInit_spam);
+  Py_Initialize();
+  PyRun_SimpleString("import spam\nprint( spam.system('echo hi') )");
+  Py_Finalize();
+
+  return cli(argc, argv);
 }
